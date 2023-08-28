74b9a6a9d0d56a39d6ff17ac537e39838e65aada
parkjiwoo <dndjf1109@gmail.com>
2023-08-28 17:24:08 +0900

    up

---
+++ accounts/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render,redirect
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib.auth import login as auth_login 
+from django.contrib.auth import logout as auth_logout
+
+# Create your views here.
+def signup(request):
+    if request.method=='POST':
+        form= CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:signup')
+            
+    else:
+        form=CustomUserCreationForm()
+
+    context={
+        'form':form,
+    }
+
+    return render(request,'signup.html',context)
+
+def login(request):
+    if request.method=='POST':
+        form=CustomAuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('accounts:login')
+
+    else:
+        form=CustomAuthenticationForm()
+
+        
+    context={
+        'form':form,
+    }
+
+    return render(request,'login.html',context)
+
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:login')
+
+
+
+        
