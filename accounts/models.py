bbf1da68c2208f404c597076c31dd0bcefca6fce
parkjiwoo <dndjf1109@gmail.com>
2023-08-29 10:07:25 +0900

    user migration

--- accounts/models.py
+++ accounts/models.py
@@ -3,3 +3,5 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):           
         pass
+        #aeticle_set =
+        #comment_set =
--- articles/models.py
+++ articles/models.py
@@ -1,7 +1,26 @@
 from django.db import models
+from accounts.models import User
+from django.conf import settings
+from django.contrib.auth import get_user_model 
 
 # Create your models here.
 class Article(models.Model):
-    title= models.CharField(max_length=50)
-    content= models.TextField()
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    
+    # user=models.Foreignkey(User,on_delete=,odels.CASCADE)
+
+    # user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    user=models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+
+    user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+
    
